f9ec60ac9ed49ea72ae0321a4a8e5b5af2cf9e7e
ShinyMegaGardevoir <sadliergirls@gmail.com>
2014-10-07 09:50:27 -0600

    Forgot to save
    Still first project, just forgot to save before I published it.

--- gen/com/goodmorning/controller/R.java
+++ gen/com/goodmorning/controller/R.java
@@ -1232,6 +1232,7 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int app_name=0x7f0a000d;
         public static final int greetings=0x7f0a000e;
+        public static final int otherVariable=0x7f0a000f;
     }
     public static final class style {
         /** 
